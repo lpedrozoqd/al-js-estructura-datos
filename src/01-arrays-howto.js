1ea9ea745982fe16868598470b69af590e597745
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-09-09 18:00:48 -0500

    Simulando arrays con objetos basados en clases

---
+++ src/01-arrays-howto.js
@@ -0,0 +1,19 @@
+class MyArray{
+    constructor(){
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index){
+        return this.data[index];
+    }
+
+    push(item){
+        this.data[this.length] = item;
+        this.length += 1;
+        return this.data;
+
+    }
+}
+
+const myArray = new MyArray();
